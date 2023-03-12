574e6d11ecb636ecf447fce10a3916df62fcb1c9
Artem007 <artem.zagrebelny@yandex.ru>
2023-03-12 15:45:29 +0300

    Добавлена Задача 2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Написать программу вывода на экран чисел следующим образом:
+//16  15.5  16.8
+//17  16.5  17.8
+//...
+//24  23.5  24.8
+
+double a = 16;
+double b = 15.5;
+double c= 16.8;
+while (a < 25)
+{
+    Console.WriteLine("{0}  {1}  {2}", a, b, c);
+
+    a++;
+    b++;
+    c++;
+}
+Console.WriteLine();
+
+a=16;
+b=15.5;
+c=16.8;
+
+for (int i = 0; i < 9; i++)
+{
+    Console.WriteLine("{0}  {1}  {2}", a, b, c);
+    
+}
+
+do
+{
+    Console.WriteLine("{0}  {1}  {2}", a, b, c);
+    a++;
+    b++;
+    c++;
+}
+while (a < 25);
+
